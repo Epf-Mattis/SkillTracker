1a17fcd98ab87a54ca6898571e4e7827552c2a09
Mattis Kohen <109628693+Neytoooo@users.noreply.github.com>
2024-10-09 15:48:55 +0200

    Ajout de la page home

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterForm';
 import LoginPage from './pages/LoginForm';
 import DashboardPage from './pages/Dashboard';
+import HomePage from './pages/Home';  // Importer la page Home
 import PrivateRoute from './components/PrivateRoute';  // Importer le composant PrivateRoute
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RegisterPage />} />        {/* Page d'inscription par défaut */}
+        <Route path="/" element={<HomePage />} />        {/* Page d'accueil par défaut */}
+        <Route path="/register" element={<RegisterPage />} />  {/* Page d'inscription */}
         <Route path="/login" element={<LoginPage />} />      {/* Page de connexion */}
 
         {/* Protéger la route du Dashboard */}
